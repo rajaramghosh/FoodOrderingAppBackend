75fde09479e1a5276ef3f8dbad1ef380bad4ea75
Adarsha Acharya <adarshrya@users.noreply.github.com>
2021-05-04 03:43:08 +0530

    Code Changes for Category Controller

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -4,6 +4,7 @@ import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,20 @@ public class CategoryService {
         return restaurantEntity.getCategories().stream()
                 .sorted(Comparator.comparing(CategoryEntity::getCategoryName))
                 .collect(Collectors.toList());
+    }
+
+    //List all categories - sorted alphabetically by categor name
+    public List<CategoryEntity> getAllCategoriesOrderedByName() {
+        return categoryDao.getAllCategoriesOrderedByName();
+    }
+
+    //Get a category entity by category UUID
+    public CategoryEntity getCategoryById(String categoryUuid) throws CategoryNotFoundException {
+        if (categoryUuid.equals("")) {
+            throw new CategoryNotFoundException("CNF-001", "Category id field should not be empty");
+        }
+
+        CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(categoryUuid);
+        return categoryEntity;
     }
 }

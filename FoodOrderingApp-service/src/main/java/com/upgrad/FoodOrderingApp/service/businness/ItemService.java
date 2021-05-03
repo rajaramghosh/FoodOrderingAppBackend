acc816d3feba7c7f583e1a08dabd548bb0ff08c7
Adarsha Acharya <adarshrya@users.noreply.github.com>
2021-05-04 03:23:58 +0530

    Code Changes for ItemController

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,25 +1,29 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
-import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
-import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
-import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
-import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
-import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.dao.*;
+import com.upgrad.FoodOrderingApp.service.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    private ItemDao itemDao;
 
     @Autowired
     private RestaurantDao restaurantDao;
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private OrderItemDao orderItemDao;
 
     public List<ItemEntity> getItemsByCategoryAndRestaurant(String restaurantUUID, String categoryUUID) {
         RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUuid(restaurantUUID);
@@ -36,5 +40,47 @@ public class ItemService {
         restaurantItemEntityList.sort(Comparator.comparing(ItemEntity::getitemName));
 
         return restaurantItemEntityList;
+    }
+
+    public List<ItemEntity> getItemsByPopularity(RestaurantEntity restaurantEntity) {
+        List<ItemEntity> itemEntityList = new ArrayList<>();
+        for (OrdersEntity orderEntity : orderDao.getOrdersByRestaurant(restaurantEntity)) {
+            for (OrderItemEntity orderItemEntity : orderItemDao.getItemsByOrders(orderEntity)) {
+                itemEntityList.add(orderItemEntity.getItem());
+            }
+        }
+
+
+        //Get all items ordered from the particular restaurant
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (ItemEntity itemEntity : itemEntityList) {
+            Integer count = map.get(itemEntity.getUuid());
+            map.put(itemEntity.getUuid(), (count == null) ? 1 : count + 1);
+        }
+
+        //Store the entire map data in treemap
+        Map<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+        List<Map.Entry<String, Integer> > list =
+                new LinkedList<Map.Entry<String, Integer> >(treeMap.entrySet());
+
+        //Sort the map entries based on number of orders - ascending
+        //Store in a list
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        //Transfer list data to  List<ItemEntity>
+        List<ItemEntity> sortedItemEntityList = new ArrayList<ItemEntity>();
+        for(int i=0;i<list.size();i++){
+            sortedItemEntityList.add(itemDao.getItemByUUID(list.get(i).getKey()));
+        }
+
+        // Reverse the List<ItemEntity> to sort by number of orders - descending order
+        Collections.reverse(sortedItemEntityList);
+        return sortedItemEntityList;
     }
 }
